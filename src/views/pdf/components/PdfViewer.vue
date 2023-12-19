67b12004033ca486efa8b6bb0d1daafd9b0cee1c
xkccoding <xkccoding@gmail.com>
2023-12-19 22:36:37 +0800

    chore: desc001

--- src/views/pdf/components/PdfViewer.vue
+++ src/views/pdf/components/PdfViewer.vue
@@ -1,37 +1,22 @@
-<template>
-
-
-
-
-    <div class="app-content">
-    <vue-pdf-embed
-      ref="pdfRef"
-      :source="pdfSource"
-      :page="page"
-      @password-requested="handlePasswordRequest"
-      @rendered="handleDocumentRender"
-    />
-  </div>
-</template>
-
 <script>
 import VuePdfEmbed from 'vue-pdf-embed'
-
 // OR THE FOLLOWING IMPORT FOR VUE 2
 // import VuePdfEmbed from 'vue-pdf-embed/dist/vue2-pdf-embed'
 
 export default {
   components: {
-    VuePdfEmbed
+    VuePdfEmbed,
   },
   data() {
     return {
       isLoading: true,
       page: null,
       pageCount: 1,
+      // pdfSource:
+      //   'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf',
       pdfSource:
-        'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf',
-      showAllPages: true
+        '/p001.pdf',
+      showAllPages: true,
     }
   },
   // watch: {
@@ -47,7 +32,19 @@ export default {
     },
     handlePasswordRequest(callback, retry) {
       callback(prompt(retry ? 'Enter password again' : 'Enter password'))
-    }
-  }
+    },
+  },
 }
 </script>
+
+<template>
+  <div class="app-content">
+    <VuePdfEmbed
+      ref="pdfRef"
+      :source="pdfSource"
+      :page="page"
+      @password-requested="handlePasswordRequest"
+      @rendered="handleDocumentRender"
+    />
+  </div>
+</template>
